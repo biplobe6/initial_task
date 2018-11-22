ee8998769eeec18014cf7884b68b941e8bfc2f9f
Md. Iubullah Biplobe <biplobe6@gmail.com>
2018-11-22 18:03:20 +0600

    Added: frontend initial data
    Added: frontend initial data

--- src/main/urls.py
+++ src/main/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'api/v1/', include('api.urls'), name="api_v1")
+    url(r'^api/v1/', include('api.urls'), name="api_v1"),
+    url(r'^$', views.home, name="home"),
+    url(r'^user', views.home, name="user")
 ]
---
+++ src/staticfiles/script/app.module.js
@@ -0,0 +1,4 @@
+
+angular.module("userApp", [
+    "userList"
+]);
